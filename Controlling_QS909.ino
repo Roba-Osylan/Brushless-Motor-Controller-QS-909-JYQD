0ad818d75ba20e2011f7663b3af5cccd7ca858cc
Roba-Osylan <85955049+Roba-Osylan@users.noreply.github.com>
2021-08-06 16:26:44 +0300

    Update Controlling_QS909.ino

--- Controlling_QS909.ino
+++ Controlling_QS909.ino
@@ -1,31 +1,31 @@
 
 //1 Wheel 
 int EL= 5; //Start=High, Stop=Low
-int ZF= 4; //Direction
+int ZF= 4; //Direction CW=High, CCW=Low
 int Signal= 3;//PWM Signal
 int VR_Speed= 2;
 
 int Rotation=0;
 
 void setup() {
-  pinMode(EL, OUTPUT); //Enable pin 
+  pinMode(EL, OUTPUT); 
+  pinMode(ZF, OUTPUT); 
   pinMode(Signal, INPUT);
-  pinMode(ZF, OUTPUT); //direction: CW (High), CCW (Low)
-
+  pinMode(VR, OUTPUT); 
 }
 
 void loop() {
+  digitalWrite(EL, HIGH); //Start
+  analogWrite(VR_Speed, 100) //Speed 
 
-  analogWrite(VR_Speed, 100) //Speed Range= 
-
-   for (Rotation = 0; Rotation <= 30; Rotation += 1) { 
-    // in steps of 1 degree
+   for (Rotation = 0; Rotation <= 180; Rotation += 1) { 
+   
     digitalWrite(ZF, HIGH); //CW rotation           
     delay(15);                      
   }
-  for (Rotation = 30; Rotation >= 0; Rotation -= 1) { 
+  for (Rotation = 30; Rotation >= 180; Rotation -= 1) { 
     digitalWrite(ZF, LOW); //CCW rotation             
     delay(15);                       
-
-
+  }
+  
 }
